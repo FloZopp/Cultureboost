28a686a14bbc357479e16bd6acae54c08c0e0ce5
FloZopp <56434596+FloZopp@users.noreply.github.com>
2026-05-23 12:06:56 +0200

    Clean up LibffiRecipe class
    Removed old docstring and unnecessary patches.

--- p4a-recipes/libffi/__init__.py
+++ p4a-recipes/libffi/__init__.py
@@ -2,13 +2,8 @@ from pythonforandroid.recipes.libffi import LibffiRecipe as _ParentLibffi
 
 
 class LibffiRecipe(_ParentLibffi):
-    """
-    Recette libffi officielle de p4a, epinglee en version 3.4.6 et sans
-    les patches de l'ancienne version (incompatibles avec la 3.4.6).
-    """
     version = "3.4.6"
     url = "https://github.com/libffi/libffi/releases/download/v{version}/libffi-{version}.tar.gz"
-    patches = []
 
 
 recipe = LibffiRecipe()
